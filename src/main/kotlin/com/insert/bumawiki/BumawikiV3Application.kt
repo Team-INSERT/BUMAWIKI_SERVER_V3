e825a34c326c5859b84ca84ca3bf74893e6797a7
qlido <jason7530@naver.com>
2024-03-11 15:08:21 +0900

    initial commit

---
+++ src/main/kotlin/com/insert/bumawiki/BumawikiV3Application.kt
@@ -0,0 +1,11 @@
+package com.insert.bumawiki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BumawikiV3Application
+
+fun main(args: Array<String>) {
+    runApplication<BumawikiV3Application>(*args)
+}
---
+++ src/test/kotlin/com/insert/bumawiki/BumawikiV3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.insert.bumawiki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BumawikiV3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
